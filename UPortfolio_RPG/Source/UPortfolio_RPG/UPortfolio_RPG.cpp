8a51a7df3145be20d566169ca9a71b30394deff1
HomePC <coxe2754@gmail.com>
2024-04-26 11:48:37 +0900

    +C++ Project

---
+++ UPortfolio_RPG/Source/UPortfolio_RPG/UPortfolio_RPG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UPortfolio_RPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UPortfolio_RPG, "UPortfolio_RPG" );
